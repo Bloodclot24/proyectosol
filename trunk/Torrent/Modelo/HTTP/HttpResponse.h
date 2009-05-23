26fb837d4dcddd2738654eaea9588c59c3027251
LUCASHPANDOLFO <LUCASHPANDOLFO@a336475a-4023-11de-9c03-5d6cfad8e937>
2009-05-23 16:55:10 +0000

    Se separo el codigo HTTP en distintos modulos. El caso de prueba se paso a un archivo separado.

---
+++ trunk/Torrent/Modelo/HTTP/HttpResponse.h
@@ -0,0 +1,29 @@
+#ifndef HTTPRESPONSE_H_INCLUDED
+#define HTTPRESPONSE_H_INCLUDED
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+
+/* Clase para parsear la respuesta HTTP */
+class HttpResponse{
+private:
+     std::string response;
+     int offsetVersion, offsetCode;
+     int longitudContenido;
+     int offsetContenido;
+     
+public:
+     /* dado un string con la respuesta, lo parsea obteniendo los
+      * datos que nos interesan */
+     HttpResponse(std::string respuesta);
+
+     /* Devuelve los datos que nos interesan */
+     std::string getContent();
+
+     /* Dado un string codificado en URL, decodifica los datos */
+     static std::string UrlDecode(std::string datos);
+};
+
+#endif //HTTPRESPONSE_H_INCLUDED
