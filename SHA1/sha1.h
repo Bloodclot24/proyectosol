80e81d19bf91ba3059a5646df96d481fa6f32d59
gygounet@gmail.com <gygounet@gmail.com@a336475a-4023-11de-9c03-5d6cfad8e937>
2009-05-24 20:13:00 +0000

    Subo la cascara del SHA 1, no compila ni esta funcionando, estoy solucionando el tema del big-endian y un detalle del preprocesamiento... al menos subo la cascara asi ya queda el archivo... cuando termine vuelvo a subir y subo un par de test tambien

---
+++ SHA1/sha1.h
@@ -0,0 +1,16 @@
+#ifndef SHA1_H_
+#define SHA1_H_
+
+#include <iostream>
+
+class Sha1
+{
+	private:
+		
+	public:
+		Sha1();
+		std::string ejecutarSha1(std::string cadena);
+		~Sha1();
+};
+
+#endif /*SHA1_H_*/
