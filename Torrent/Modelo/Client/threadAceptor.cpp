d209a1e368da77fe8265deb6bf07b35e811a67f5
gygounet@gmail.com <gygounet@gmail.com@a336475a-4023-11de-9c03-5d6cfad8e937>
2009-06-18 17:24:15 +0000

    Se agrego el thread Aceptor, se agrego un constructor al peer que recibe un socket como parametro y se lo utiliza para crear los peers entrantes, se agrego el atributo entrante a la clase peer para distinguir entre un peer entrante y uno no entrante y se incorporo el thread aceptor al cliente.

---
+++ Torrent/Modelo/Client/threadAceptor.cpp
@@ -0,0 +1,31 @@
+#include "threadAceptor.h"
+
+ThreadAceptor::ThreadAceptor(Socket *socket){
+     this->socket = socket;
+     corriendo = false;
+}
+
+void ThreadAceptor::comenzar(){
+     corriendo = true;
+     start();
+}
+
+void ThreadAceptor::finalizar(void){
+     corriendo = false;
+     socket->setTimeout(0,1);
+     socket->cerrar();
+}
+
+void ThreadAceptor::run(){
+     while(corriendo){
+	  Socket *sck=NULL;
+	  sck = socket->aceptar();  //Acepto el peer
+	  
+	  Peer *peer = new Peer(sck); //creo el peer
+	  peer->start(""); //comienza el procesamiento
+     }
+}
+
+ThreadAceptor::~ThreadAceptor(){
+     delete socket;
+}
---
+++ Torrent/Modelo/Client/threadAceptor.h
@@ -0,0 +1,29 @@
+#ifndef THREADACEPTOR_H_
+#define THREADACEPTOR_H_
+
+class  ThreadAceptor;
+
+#include "socket.h"
+#include "threads.h"
+#include "peer.h"
+
+class ThreadAceptor:Thread{
+private:
+     bool corriendo;
+     Socket *socket;
+     
+     
+public:
+     ThreadAceptor(Socket *socket);
+
+     void comenzar();
+
+     void finalizar(void);
+
+     void run();
+
+     ~ThreadAceptor();
+
+};
+   
+#endif /*THREADACEPTOR_H_*/
